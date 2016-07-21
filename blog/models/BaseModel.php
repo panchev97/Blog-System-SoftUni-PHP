ae6d5097ea5436f89e3234b712e8ef3ce161eb3a
PTBGNR <rpanchev9708@gmail.com>
2016-07-21 10:47:46 +0300

    Update BaseModel.php

--- blog/models/BaseModel.php
+++ blog/models/BaseModel.php
@@ -10,7 +10,7 @@ abstract class BaseModel
             self::$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
             self::$db->set_charset("utf8");
             if (self::$db->connect_errno) {
-                die('Cannot connect to database');
+                die('Cannot connect to database');;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             }
         }
     }
