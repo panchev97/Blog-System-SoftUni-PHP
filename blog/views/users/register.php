4b1f04ee50c955ac291f205ec6abb413d96e9d88
PTBGNR <rpanchev9708@gmail.com>
2016-07-21 15:10:28 +0300

    Update register.php

--- blog/views/users/register.php
+++ blog/views/users/register.php
@@ -2,4 +2,17 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<!-- TODO: register form will come here ... -->
+<form method="post">
+    <div class="conteiner">
+    <div class="register-form-input">
+        <input type="text" name="username" placeholder="Enter Username" />
+    </div>
+    <div class="register-form-input">
+        <input type="password" name="password" placeholder="Enter Password">
+    </div>
+    <div class="register-form-input">
+        <input type="text" name="fullname" placeholder="Enter Full Name">
+        <input type="submit" name="submit" value="Register" class="btn-login"><br>
+    </div>
+    </div>
+</form>
