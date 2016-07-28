34543211a5c4282681e6c5464f69ce77aeb96b8f
PTBGNR <rpanchev9708@gmail.com>
2016-07-28 15:03:27 +0300

    Update edit.php

--- blog/views/posts/edit.php
+++ blog/views/posts/edit.php
@@ -4,17 +4,13 @@
 
 <form method="post">
     <div class="second-container">
-    <div>Title:</div>
     <input type="text" name="post_title"
-           value="<?=htmlspecialchars($this->post['title'])?>" class="new-form-input">
-    <div>Content:</div>
-    <textarea rows="10" name="post_content"
+           value="<?=htmlspecialchars($this->post['title'])?>" class="new-form-input" placeholder="Title">
+    <textarea rows="10" name="post_content" placeholder="Content"
     ><?=htmlspecialchars($this->post['content'])?></textarea>
-    <div>Date (yyyy-MM-dd hh:mm:ss):</div>
-    <input type="datetime" name="post_date"
+    <input type="datetime" name="post_date" placeholder="Date"
            value="<?=htmlspecialchars($this->post['date'])?>">
-    <div>Author ID:</div>
-    <input type="text" name="post_user_id"
+    <input type="text" name="post_user_id" placeholder="Author ID"
            value="<?=htmlspecialchars($this->post['user_id'])?>">
     <div><input type="submit" value="Edit post">
         <a href="<?=APP_ROOT?>/posts">[Cancel]</a></div>
