986202cae3151f030f1dec9c5d4f5b4cf065c1f7
PTBGNR <rpanchev9708@gmail.com>
2016-08-03 16:01:23 +0300

    Visits added!

--- blog/views/home/view.php
+++ blog/views/home/view.php
@@ -1,11 +1,19 @@
 <?php $this->title = $this->post['title']; ?>
 
 <h1><?=htmlspecialchars($this->title)?></h1>
+<?php
+if (!isset($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+} else {
+    $_SESSION['count']++;
+}
+echo "Visits:" . $_SESSION['count'];
+?>
 
 <main id="posts">
     <article>
-        <div class="date"><i>Posted on</i>
-            <?=(new DateTime($this->post['date']))->format('d-M-Y')?><i>by</i><?=htmlentities($this->post['full_name'])?>
+        <div class="date"><i>Posted on </i>
+            <?=(new DateTime($this->post['date']))->format('d-M-Y')?><i>by </i><?=htmlentities($this->post['full_name'])?>
         </div>
         <p class="content"><?=$this->post['content']?></p>
     </article>
