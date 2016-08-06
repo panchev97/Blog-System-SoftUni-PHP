527b08f9c8c22671ca3b353ba9de3da5a2cbff19
PTBGNR <rpanchev9708@gmail.com>
2016-08-06 15:19:29 +0300

    Update index.php

--- blog/views/home/index.php
+++ blog/views/home/index.php
@@ -1,22 +1,24 @@
-<?php $this->title = 'Welcome to My Blog'; ?>
+<?php $this->title ='Welcome to My Blog'; ?>
 
 <h1><?=htmlspecialchars($this->title)?></h1>
 
-<aside>
+<aside class="asideClass">
     <h2>Recent Posts</h2>
     <?php foreach ($this->sidebarPosts as $post) : ?>
         <a href="<?=APP_ROOT?>/home/view/<?=$post['id']?>"><?=htmlentities($post['title'])?></a>
     <?php endforeach ?>
 </aside>
-<main id="posts">
-    <article>
-        <?php foreach ($this->posts as $post) : ?>
-            <h2 class="title"><?=htmlentities($post['title'])?></h2>
-            <div class="date"><i>Posted on</i>
-                <?=(new DateTime($post['date']))->format('d-M-Y') ?>
-                <i>by</i><?=htmlentities($post['full_name'])?>
-            </div>
-            <p class="content"><?=$post['content']?></p>
-        <?php endforeach ?>
-    </article>
-</main>
+<table align="center">
+    <main id="posts">
+        <article>
+            <?php foreach ($this->posts as $post) : ?>
+                <center><h2 class="title"><?=htmlentities($post['title'])?></h2> </center>
+                <div class="date"><i>Posted on</i>
+                    <?=(new DateTime($post['date']))->format('d-M-Y') ?>
+                    <i> by </i><?=htmlentities($post['full_name'])?>
+                </div>
+                <center><p class="content"><?=$post['content']?></p></center>
+            <?php endforeach ?>
+        </article>
+    </main>
+</table>
